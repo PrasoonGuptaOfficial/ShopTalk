b98096ab1b0bb7757870fc6ec57d65e7029b74fe
Prasoon Gupta <PrasoonguptaOfficial@gmail.com>
2025-04-27 01:40:21 +0530

    Home Page Banner Added

--- Frontend/Pages/HomeFlow/HomePage.tsx
+++ Frontend/Pages/HomeFlow/HomePage.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  ImageBackground,
+  Text,
+} from 'react-native';
 
 function HomePage(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.homePage}>
-      <Text>Home Page</Text>
+      <ImageBackground
+        alt="Shop Talk Shopping Banner"
+        blurRadius={10}
+        borderBottomLeftRadius={10}
+        borderBottomRightRadius={10}
+        crossOrigin="use-credentials"
+        loadingIndicatorSource={require('../../Assets/Images/ShopTalkShoppingBanner.png')}
+        resizeMode="cover"
+        resizeMethod="auto"
+        source={require('../../Assets/Images/ShopTalkShoppingBanner.png')}
+        style={styles.shoppingBanner}
+        testID="Shop_Talk_Shopping_Banner">
+        <Text>Text 1</Text>
+        <Text>Text 2</Text>
+        <Text>Text 3</Text>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
@@ -13,6 +34,13 @@ const styles = StyleSheet.create({
   homePage: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  shoppingBanner: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height / 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
 
--- Frontend/PagesNavigation/MainNavigator.tsx
+++ Frontend/PagesNavigation/MainNavigator.tsx
@@ -3,7 +3,7 @@ import LoginRegisterNavigator from './LoginRegisterNavigator';
 import HomeNavigator from './HomeNavigator';
 
 function MainNavigator(): React.JSX.Element {
-  let loginFlag = false;
+  let loginFlag = true;
   return loginFlag ? <HomeNavigator /> : <LoginRegisterNavigator />;
 }
 
